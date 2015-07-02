744707ed9aaf78656ec1dfae0cff77ad64e3a2f0
coopci <bo.coopci@gmail.com>
2015-07-02 17:00:48 +0800

    handler PING, but not sending PONG.

--- src/java/gubo/mina/websocket/WebSocketFilter.java
+++ src/java/gubo/mina/websocket/WebSocketFilter.java
@@ -41,38 +41,115 @@ public class WebSocketFilter extends IoFilterAdapter {
                 // as we want to abstract the handshake request message from the handler.
                 in.position(in.limit());
                 return ;
-            }
-            else{
+            } else{
                 // message is from a native socket. Simply wrap and pass through.
                 resultBuffer = IoBuffer.wrap(in.array(), 0, in.limit());
                 in.position(in.limit());
                 session.setAttribute(WebSocketUtils.SessionAttribute, false);
             }
-        }
-        else if(session.containsAttribute(WebSocketUtils.SessionAttribute) && true==(Boolean)session.getAttribute(WebSocketUtils.SessionAttribute)){            
+        } else if(session.containsAttribute(WebSocketUtils.SessionAttribute) && true==(Boolean)session.getAttribute(WebSocketUtils.SessionAttribute)){            
             // there is incoming data from the websocket. Decode and send to handler or next filter.     
             int startPos = in.position();
-            resultBuffer = buildWSDataBuffer(in, session);
-            if(resultBuffer == null){
+            // resultBuffer = buildWSDataBuffer(in, session);
+            ParsedFrame parsedFrame = buildWSDataBuffer(in, session);
+            if(parsedFrame == null){
                 // There was not enough data in the buffer to parse. Reset the in buffer
                 // position and wait for more data before trying again.
                 in.position(startPos);
                 return ;
             }
-        }
-        else{
+            if (parsedFrame.opCode == 9) { // PING
+            	// do nothing instead of sending a PONG. It works fine with firefox 38.0.5
+            	// TODO send a PONG frame
+            	return;
+            } else if (parsedFrame.opCode == 0xA) { // PONG
+            	return;
+            }
+            resultBuffer = parsedFrame.unMaskedPayLoad;
+        } else {
             // session is known to be from a native socket. So
             // simply wrap and pass through.
             resultBuffer = IoBuffer.wrap(in.array(), 0, in.limit());    
             in.position(in.limit());
         }                  
         // out.write(resultBuffer);
-        nextFilter.messageReceived(session, resultBuffer);
+		nextFilter.messageReceived(session, resultBuffer);
         return ;
     }
     
+    static public class ParsedFrame {
+    	byte opCode;
+    	IoBuffer unMaskedPayLoad; 
+    }
     
-    private static IoBuffer buildWSDataBuffer(IoBuffer in, IoSession session) {
+    private static ParsedFrame buildWSDataBuffer(IoBuffer in, IoSession session) {
+    	ParsedFrame parsedFrame = new ParsedFrame();
+        IoBuffer resultBuffer = null;
+        do{
+            byte frameInfo = in.get();            
+            byte opCode = (byte) (frameInfo & 0x0f);
+            parsedFrame.opCode = opCode;
+            if (opCode == 8) {
+                // opCode 8 means close. See RFC 6455 Section 5.2
+                // return what ever is parsed till now.
+                session.close(true);
+                return null;
+            }
+            
+            if (opCode == 9) { // PING
+            	// TODO handler ping frame
+            	return parsedFrame;
+            }
+            int frameLen = (in.get() & (byte) 0x7F);
+            if(frameLen == 126){
+                frameLen = in.getShort();
+            }
+            
+            // Validate if we have enough data in the buffer to completely
+            // parse the WebSocket DataFrame. If not return null.
+            if(frameLen+4 > in.remaining()){                
+                return null;
+            }
+            byte mask[] = new byte[4];
+            for (int i = 0; i < 4; i++) {
+                mask[i] = in.get();
+            }
+
+            /*  now un-mask frameLen bytes as per Section 5.3 RFC 6455
+                Octet i of the transformed data ("transformed-octet-i") is the XOR of
+                octet i of the original data ("original-octet-i") with octet at index
+                i modulo 4 of the masking key ("masking-key-octet-j"):
+
+                j                   = i MOD 4
+                transformed-octet-i = original-octet-i XOR masking-key-octet-j
+            * 
+            */
+             
+            byte[] unMaskedPayLoad = new byte[frameLen];
+            for (int i = 0; i < frameLen; i++) {
+                byte maskedByte = in.get();
+                unMaskedPayLoad[i] = (byte) (maskedByte ^ mask[i % 4]);
+            }
+            
+            if(resultBuffer == null){
+                resultBuffer = IoBuffer.wrap(unMaskedPayLoad);
+                resultBuffer.position(resultBuffer.limit());
+                resultBuffer.setAutoExpand(true);
+            }
+            else{
+                resultBuffer.put(unMaskedPayLoad);
+            }
+        }
+        while(in.hasRemaining());
+        
+        resultBuffer.flip();
+        parsedFrame.unMaskedPayLoad = resultBuffer;
+        return parsedFrame;
+
+    }    
+    
+    
+    private static IoBuffer buildWSDataBufferOld(IoBuffer in, IoSession session) {
 
         IoBuffer resultBuffer = null;
         do{
@@ -83,7 +160,11 @@ public class WebSocketFilter extends IoFilterAdapter {
                 // return what ever is parsed till now.
                 session.close(true);
                 return resultBuffer;
-            }        
+            }
+            
+            if (opCode == 9) { // PING
+            	// TODO handler ping frame
+            }
             int frameLen = (in.get() & (byte) 0x7F);
             if(frameLen == 126){
                 frameLen = in.getShort();
@@ -165,15 +246,16 @@ public class WebSocketFilter extends IoFilterAdapter {
              
              Map<String, String> headers = WebSocketUtils.parseRequest(payLoadMsg);
              
-             if (!this.permissionChecker.check(headers, session)) {
-            	 WebSocketHandShakeResponse wsResponse = WebSocketUtils.buildForbiddenResponse();
-            	 sendReponse(wsResponse, session, nextFilter);
-            	 return true;
-             }
+             
              String socketKey = headers.get("Sec-WebSocket-Key");
              
              if(socketKey == null || socketKey.length() <= 0){
                  return false;
+             }
+             if (!this.permissionChecker.check(headers, session)) {
+            	 WebSocketHandShakeResponse wsResponse = WebSocketUtils.buildForbiddenResponse();
+            	 sendReponse(wsResponse, session, nextFilter);
+            	 return true;
              }
              String challengeAccept = WebSocketUtils.getWebSocketKeyChallengeResponse(socketKey);            
              WebSocketHandShakeResponse wsResponse = WebSocketUtils.buildWSHandshakeResponse(challengeAccept);
@@ -194,11 +276,15 @@ public class WebSocketFilter extends IoFilterAdapter {
      @Override
      public void filterWrite(NextFilter nextFilter, IoSession session, WriteRequest request) {
     	 
-    	 IoBuffer resultBuffer = WebSocketFilter.buildWSDataFrameBuffer((IoBuffer)request.getMessage());
-    	 nextFilter.filterWrite(
-    	        session, 
-    	        new DefaultWriteRequest(resultBuffer, request.getFuture(), request.getDestination())
-    	        );
+    	 if(session.containsAttribute(WebSocketUtils.SessionAttribute) && true==(Boolean)session.getAttribute(WebSocketUtils.SessionAttribute)){
+	    	 IoBuffer resultBuffer = WebSocketFilter.buildWSDataFrameBuffer((IoBuffer)request.getMessage());
+	    	 nextFilter.filterWrite(
+	    	        session, 
+	    	        new DefaultWriteRequest(resultBuffer, request.getFuture(), request.getDestination())
+	    	        );
+    	 } else {
+    		 nextFilter.filterWrite(session, request);
+    	 }
     }
      
   // Encode the in buffer according to the Section 5.2. RFC 6455
